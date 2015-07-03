ecfe37867edbe2ad5eb861a42d49d53ea04ea546
张怀政 <mishuzhang33@126.com>
2015-07-04 00:08:15 +0800

    update

--- stock_PLR.py
+++ stock_PLR.py
@@ -11,18 +11,20 @@ import matplotlib.font_manager as font_manager
 import stock_function as sf
 import numpy as np
 
+
 def PLR(arr,start,end,thd):
-    i = start+1
-    j = start+1
-    k = end-1
-    temp_left = arr[start]
+    i = 0
+    j = 0
+    k = end
+    temp_left = arr[0]
     temp_right = arr[end]
     max = 0
-    temp_max = arr[:,0][start]
+    temp_max = arr[:,0][0]
+
     global global_PLR
 
     while arr[:,0][i] < end:
-        d = sf.distance(arr[:,0][i], arr[:,2][i], arr[:,0][start], arr[:,2][start], arr[:,0][end], arr[:,2][end])
+        d = sf.distance(arr[:,0][i], arr[:,2][i], arr[:,0][0], arr[:,2][0], arr[:,0][end], arr[:,2][end])
         if d > max:
             max = d
             temp_max = arr[:,0][i]
@@ -30,30 +32,51 @@ def PLR(arr,start,end,thd):
 
     global_PLR = np.vstack((global_PLR,arr[temp_max]))
 
-    while arr[:,0][j] < temp_max:
+    while arr[:,0][j] <= temp_max:
         temp_left = np.vstack((temp_left, arr[j]))
         j = j+1
 
-    while arr[:,0][k] > temp_max:
-        tem_right = np.vstack((arr[k], temp_right))
+    while arr[:,0][k] >= temp_max:
+        temp_right = np.vstack((arr[k], temp_right))
         k = k-1
 
+    print temp_left
+    print temp_right
+    print max
+    print temp_max
+    print start
+    print end
+    print thd
+
     if max >= thd:
-        PLR(temp_left, start, temp_max, thd)
-        PLR(temp_right, temp_max, end, thd)
+        if (temp_max - start) < 3:
+            return
+        else:
+            left = temp_left
+            PLR(left, start, temp_max, thd)
+
+        """if (end - temp_max) < 3:
+            return
+        else:
+            right = temp_right
+            PLR(right, temp_max, (end - temp_max), thd)
     else:
-        return
+        return"""
 
 stock_code = raw_input("Please input your stockcode:")
 xdata, ndata, stock_data = sf.handle_data(stock_code)
 
-
+m,n = np.split(stock_data, [stock_data[:,0][9]])
+print m[0]
+print n[9]
 global_PLR = np.vstack((stock_data[ndata[1]][-1], stock_data[ndata[1]][0]))
-
 PLR(stock_data, stock_data[ndata[1],0][-1], stock_data[ndata[1],0][0], 3)
-print global_PLR
 
-"""plt.rc('axes', grid=True)
+p = np.array(sorted(global_PLR,key = lambda global_PLR: global_PLR[0]))
+
+
+
+plt.rc('axes', grid=True)
 plt.rc('grid', color='0.75', linestyle='-', linewidth=0.5)
 textsize = 9
 left, width = 0.1, 0.8
@@ -65,10 +88,9 @@ axescolor  = '#f6f6f6'  # the axes background color
 ax1 = fig.add_axes(rect1, axisbg=axescolor)
 ax1.set_xticklabels(xdata[::20], rotation=45, horizontalalignment='right')
 ax1.set_xticks(stock_data[ndata[1],0][::-20])
-ax1.plot(stock_data[ndata[1],0][::-1], stock_data[ndata[1],6], lw=0.5, label='average_price')
+ax1.plot(stock_data[ndata[1],0][::-1], stock_data[ndata[1],6], lw=1, color="g", label='average_price')
 ax1.plot(stock_data[ndata[1],0][::-1], stock_data[ndata[1],2][::-1], lw=2.0, color="r", label='close_price')
-ax1.plot([stock_data[ndata[1],0][-1],stock_data[ndata[1],0][0]],
-         [stock_data[ndata[1],6][0],stock_data[ndata[1],6][-1]])
+ax1.plot(p[:,0][::-1], p[:,2][::-1],lw=2.0, color="b",label='PLR')
 
 props = font_manager.FontProperties(size=10)
 leg = ax1.legend(loc='best', shadow=True, fancybox=True, prop=props)
@@ -76,4 +98,3 @@ leg.get_frame().set_alpha(0.5)
 
 plt.title("%sPLR"%stock_code)
 plt.show()
-"""
